b29dfd1d189a8404bc87713aef0b942f79c33f50
Francois <francoisbarbe@hotmail.com>
2018-04-20 16:34:42 -0400

    Backup

---
+++ src/Home.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h3>Home.vue</h3>
+        <app-server-status v-for="server in 5"></app-server-status>
+    </div>
+</template>
+
+<script>
+    import ServerStatus from './ServerStatus.vue';
+
+    export default {
+        components: {
+            'app-server-status': ServerStatus
+        }
+    }
+
+</script>
---
+++ src/ServerStatus.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="wrapper">
+        <h3>ServerStatus.vue</h3>
+        <p>Server status: {{ status }}</p>
+        <hr>
+        <button @click="changeStatus">Change status</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        data: function() {
+            return {
+                status: 'Critical'
+            }
+        },
+        methods: {
+            changeStatus() {
+                this.status = 'Normal';
+            }
+        }
+    }
+
+</script>
+
+<style>
+    .wrapper {
+        border: 1px solid black;
+        padding: 0 1em 1em 1em;
+        max-width: 20%;
+    }
+
+</style>
